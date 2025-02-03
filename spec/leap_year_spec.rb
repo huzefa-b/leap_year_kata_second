25453199349223572beeb5d4a400dd440ac3928b
huzefa <huzefa.b@incubyte.co>
2025-02-04 00:00:16 +0530

    Add case when year is divisible by 4

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe LeapYear do
         expect(calculator.leap_year?(1997)).to be false
       end
     end
+
+    context 'when year is divisible by 4' do
+      it 'returns true' do
+        expect(calculator.leap_year?(1996)).to be true
+      end
+    end
   end
 end
