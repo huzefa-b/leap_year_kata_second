4e5bffc84c50e8fa76a9e31a812bd257d1840e1c
huzefa <huzefa.b@incubyte.co>
2025-02-04 00:02:51 +0530

    Add scenario when year is divisible by 100 but not 400

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,3 +1,5 @@
+require 'leap_year'
+
 RSpec.describe LeapYear do
   let(:calculator) { LeapYear.new }
 
@@ -11,6 +13,12 @@ RSpec.describe LeapYear do
     context 'when year is divisible by 4' do
       it 'returns true' do
         expect(calculator.leap_year?(1996)).to be true
+      end
+    end
+
+    context 'when year is divisible by 100 but not by 400' do
+      it 'returns false' do
+        expect(calculator.leap_year?(1800)).to be false
       end
     end
   end
